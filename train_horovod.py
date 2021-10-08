0d2867018743133a8da82b0ef1bb21b937a027a9
HotaekHan <HotaekHan>
2021-10-08 13:17:51 +0900

    update code

--- train_horovod.py
+++ train_horovod.py
@@ -12,11 +12,14 @@ import torchvision.transforms as transforms
 from torch.optim import lr_scheduler
 from torch import autograd
 
+# distributed
 import torch.utils.data.distributed
 import horovod.torch as hvd
 
 # 3rd-party utils
 from torch.utils.tensorboard import SummaryWriter
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 # user-defined
 from loss import FocalLoss
@@ -31,7 +34,7 @@ opt = parser.parse_args()
 
 config = utils.get_config(opt.config)
 start_epoch = 1  # start from epoch 0 or last epoch
-fp16 = True
+fp16 = False
 num_workers = 0
 
 # Horovod: initialize library.
@@ -78,35 +81,75 @@ if hvd.rank() == 0:
     summary_writer = SummaryWriter(os.path.join(config['model']['exp_path'], 'log'))
 
 # Data
-print('==> Preparing data..')
-transform = transforms.Compose([
-    transforms.ToTensor()
-])
-
-target_classes = config['params']['classes'].split('|')
+target_classes = utils.read_txt(config['params']['classes'])
+num_classes = len(target_classes)
 img_size = config['params']['image_size'].split('x')
 img_size = (int(img_size[0]), int(img_size[1]))
 
+# Data
+print('==> Preparing data..')
+bbox_params = A.BboxParams(format='pascal_voc', min_visibility=0.3)
+train_transforms = A.Compose([
+    A.Resize(height=img_size[0], width=img_size[1], p=1.0),
+    A.HorizontalFlip(p=0.5),
+    # A.OneOf([
+    #     A.Sequential([
+    #         A.Resize(height=img_size[0], width=img_size[1], p=1.0),
+    #     ], p=1.0),
+    #     A.Sequential([
+    #         A.RandomSizedBBoxSafeCrop(height=img_size[0], width=img_size[1], p=1.0),
+    #     ], p=1.0)
+    # ], p=1.0),
+
+    A.OneOf([
+        A.Sequential([
+            A.GaussNoise(var_limit=(100, 150), p=0.5),
+            A.MotionBlur(blur_limit=17, p=0.5)
+        ], p=1.0),
+        A.Sequential([
+            A.GaussNoise(var_limit=(100, 150), p=0.5),
+            A.MotionBlur(blur_limit=17, p=0.5),
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+            A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=0, p=0.5),
+        ], p=1.0),
+        A.Sequential([
+            A.GaussNoise(var_limit=(100, 150), p=0.5),
+            A.MotionBlur(blur_limit=17, p=0.5),
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+            A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=0, p=0.5),
+            A.ChannelShuffle(p=0.5),
+            # A.ShiftScaleRotate(shift_limit=0.1, scale_limit=(-0.15, 0.15), rotate_limit=30, p=0.5,
+            #                    border_mode=cv2.BORDER_CONSTANT, value=0),
+        ], p=1.0)
+    ], p=1.0),
+
+    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, p=1.0),
+    ToTensorV2()
+], bbox_params=bbox_params, p=1.0)
+valid_transforms = A.Compose([
+    A.Resize(height=img_size[0], width=img_size[1], p=1.0),
+    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, p=1.0),
+    ToTensorV2()
+], bbox_params=bbox_params, p=1.0)
+
 train_dataset = jsonDataset(path=config['data']['train'].split(' ')[0], classes=target_classes,
-                            transform=transform,
+                            transform=train_transforms,
                             input_image_size=img_size,
-                            num_crops=config['params']['num_crops'],
-                            do_aug=config['params']['do_aug'])
+                            num_crops=config['params']['num_crops'])
 
 valid_dataset = jsonDataset(path=config['data']['valid'].split(' ')[0], classes=target_classes,
-                            transform=transform,
+                            transform=valid_transforms,
                             input_image_size=img_size,
                             num_crops=config['params']['num_crops'])
 
 assert train_dataset
 assert valid_dataset
 
-if config['data']['add_train'] != 'None':
+if config['data']['add_train'] is not None:
     add_train_dataset = jsonDataset(path=config['data']['add_train'].split(' ')[0], classes=target_classes,
-                            transform=transform,
-                            input_image_size=img_size,
-                            num_crops=config['params']['num_crops'],
-                            do_aug=config['params']['do_aug'])
+                                    transform=train_transforms,
+                                    input_image_size=img_size,
+                                    num_crops=config['params']['num_crops'])
     concat_train_dataset = ConcatBalancedDataset([train_dataset, add_train_dataset])
     assert add_train_dataset
     assert concat_train_dataset
@@ -156,25 +199,19 @@ net = net.to(device)
 
 # print out net
 if hvd.rank() == 0:
-    num_parameters = 0.
-    for param in net.parameters():
-        sizes = param.size()
-
-        num_layer_param = 1.
-        for size in sizes:
-            num_layer_param *= size
-        num_parameters += num_layer_param
-    # print(net)
-    print("num. of parameters : " + str(num_parameters))
+    print(net)
+    n_parameters = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    print('number of params:', n_parameters)
 
 # loss
 criterion = FocalLoss(num_classes=num_classes)
 
 # optimizer
 if config['params']['optimizer'] == 'SGD':
-    optimizer = optim.SGD(net.parameters(), lr=float(config['params']['lr']), momentum=0.9, weight_decay=5e-4)
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, net.parameters()), lr=float(config['params']['lr']),
+                          momentum=0.9, weight_decay=5e-4)
 elif config['params']['optimizer'] == 'Adam':
-    optimizer = optim.Adam(net.parameters(), lr=float(config['params']['lr']))
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=float(config['params']['lr']))
 else:
     raise ValueError('not supported optimizer')
 
@@ -232,7 +269,7 @@ elif config['params']['lr_multistep'] != 'None':
     tmp_str += ']'
     print(tmp_str)
 print("Size of batch : " + str(train_loader.batch_size))
-print("transform : " + str(transform))
+print("transform : " + str(train_transforms))
 
 if config['data']['add_train'] != 'None':
     print("num. train data : ")
@@ -261,37 +298,46 @@ def train(epoch):
 
     with torch.set_grad_enabled(True):
         # with autograd.detect_anomaly():
-        for batch_idx, (inputs, loc_targets, cls_targets, mask_targets, paths) in enumerate(train_loader):
+        # for batch_idx, (inputs, loc_targets, cls_targets, mask_targets, paths) in enumerate(train_loader):
+        for batch_idx, (inputs, loc_targets, cls_targets, paths) in enumerate(train_loader):
             inputs = inputs.to(device)
             loc_targets = loc_targets.to(device)
             cls_targets = cls_targets.to(device)
-            mask_targets = mask_targets.to(device)
+            # mask_targets = mask_targets.to(device)
 
-            loc_preds, cls_preds, mask_preds = net(inputs)
+            # loc_preds, cls_preds, mask_preds = net(inputs)
+            loc_preds, cls_preds = net(inputs)
 
-            loc_loss, cls_loss, mask_loss, num_matched_anchors = \
-                criterion(loc_preds, loc_targets, cls_preds, cls_targets, mask_preds, mask_targets)
+            # loc_loss, cls_loss, mask_loss, num_matched_anchors = \
+            #     criterion(loc_preds, loc_targets, cls_preds, cls_targets, mask_preds, mask_targets)
+            loc_loss, cls_loss, num_matched_anchors = \
+                criterion(loc_preds, loc_targets, cls_preds, cls_targets)
             if num_matched_anchors == 0:
                 print('No matched anchor')
                 continue
             else:
                 num_matched_anchors = float(num_matched_anchors)
-                loss = ((loc_loss + cls_loss) / num_matched_anchors) + mask_loss
+                # loss = ((loc_loss + cls_loss) / num_matched_anchors) + mask_loss
+                loss = (loc_loss + cls_loss) / num_matched_anchors
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             train_loss += loss.item()
-            print('[Train#%d] epoch: %3d | iter: %4d | loc_loss: %.3f | cls_loss: %.3f | mask_loss: %.3f | '
+            # print('[Train#%d] epoch: %3d | iter: %4d | loc_loss: %.3f | cls_loss: %.3f | mask_loss: %.3f | '
+            #       'train_loss: %.3f | avg_loss: %.3f | matched_anchors: %d'
+            #       % (hvd.rank(), epoch, batch_idx, loc_loss.item(), cls_loss.item(), mask_loss.item(),
+            #          loss.item(), train_loss/(batch_idx+1), num_matched_anchors))
+            print('[Train#%d] epoch: %3d | iter: %4d | loc_loss: %.3f | cls_loss: %.3f | '
                   'train_loss: %.3f | avg_loss: %.3f | matched_anchors: %d'
-                  % (hvd.rank(), epoch, batch_idx, loc_loss.item(), cls_loss.item(), mask_loss.item(),
+                  % (hvd.rank(), epoch, batch_idx, loc_loss.item(), cls_loss.item(),
                      loss.item(), train_loss/(batch_idx+1), num_matched_anchors))
 
             if hvd.rank() == 0:
                 summary_writer.add_scalar('train/loc_loss', loc_loss.item(), global_iter_train)
                 summary_writer.add_scalar('train/cls_loss', cls_loss.item(), global_iter_train)
-                summary_writer.add_scalar('train/mask_loss', mask_loss.item(), global_iter_train)
+                # summary_writer.add_scalar('train/mask_loss', mask_loss.item(), global_iter_train)
                 summary_writer.add_scalar('train/train_loss', loss.item(), global_iter_train)
                 global_iter_train += 1
 
@@ -327,32 +373,41 @@ def valid(epoch):
     global global_iter_train
 
     with torch.set_grad_enabled(False):
-        for batch_idx, (inputs, loc_targets, cls_targets, mask_targets, paths) in enumerate(valid_loader):
+        # for batch_idx, (inputs, loc_targets, cls_targets, mask_targets, paths) in enumerate(valid_loader):
+        for batch_idx, (inputs, loc_targets, cls_targets, paths) in enumerate(valid_loader):
             inputs = inputs.to(device)
             loc_targets = loc_targets.to(device)
             cls_targets = cls_targets.to(device)
-            mask_targets = mask_targets.to(device)
+            # mask_targets = mask_targets.to(device)
 
-            loc_preds, cls_preds, mask_preds = net(inputs)
+            # loc_preds, cls_preds, mask_preds = net(inputs)
+            loc_preds, cls_preds = net(inputs)
 
-            loc_loss, cls_loss, mask_loss, num_matched_anchors = \
-                criterion(loc_preds, loc_targets, cls_preds, cls_targets, mask_preds, mask_targets)
+            # loc_loss, cls_loss, mask_loss, num_matched_anchors = \
+            #     criterion(loc_preds, loc_targets, cls_preds, cls_targets, mask_preds, mask_targets)
+            loc_loss, cls_loss, num_matched_anchors = \
+                criterion(loc_preds, loc_targets, cls_preds, cls_targets)
             if num_matched_anchors == 0:
                 print('No matched anchor')
                 continue
             num_matched_anchors = float(num_matched_anchors)
-            loss = ((loc_loss + cls_loss) / num_matched_anchors) + mask_loss
+            # loss = ((loc_loss + cls_loss) / num_matched_anchors) + mask_loss
+            loss = ((loc_loss + cls_loss) / num_matched_anchors)
             valid_loss += loss.item()
             avg_valid_loss = valid_loss / (batch_idx + 1)
-            print('[Valid#%d] epoch: %3d | iter: %4d | loc_loss: %.3f | cls_loss: %.3f | mask_loss: %.3f | '
+            # print('[Valid#%d] epoch: %3d | iter: %4d | loc_loss: %.3f | cls_loss: %.3f | mask_loss: %.3f | '
+            #       'valid_loss: %.3f | avg_loss: %.3f | matched_anchors: %d'
+            #     % (hvd.rank(), epoch, batch_idx, loc_loss.item(), cls_loss.item(), mask_loss.item(),
+            #        loss.item(), avg_valid_loss, num_matched_anchors))
+            print('[Valid#%d] epoch: %3d | iter: %4d | loc_loss: %.3f | cls_loss: %.3f | '
                   'valid_loss: %.3f | avg_loss: %.3f | matched_anchors: %d'
-                % (hvd.rank(), epoch, batch_idx, loc_loss.item(), cls_loss.item(), mask_loss.item(),
+                % (hvd.rank(), epoch, batch_idx, loc_loss.item(), cls_loss.item(),
                    loss.item(), avg_valid_loss, num_matched_anchors))
 
             if hvd.rank() == 0:
                 summary_writer.add_scalar('valid/loc_loss', loc_loss.item(), global_iter_valid)
                 summary_writer.add_scalar('valid/cls_loss', cls_loss.item(), global_iter_valid)
-                summary_writer.add_scalar('valid/mask_loss', mask_loss.item(), global_iter_valid)
+                # summary_writer.add_scalar('valid/mask_loss', mask_loss.item(), global_iter_valid)
                 summary_writer.add_scalar('valid/valid_loss', loss.item(), global_iter_valid)
                 global_iter_valid += 1
 
